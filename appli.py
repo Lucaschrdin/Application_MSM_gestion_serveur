8f2f8479bf476b0e6576b0c67f6fc6e90be4b453
Lucaschrdin <lucas.chardin@hotmail.fr>
2021-06-28 16:35:27 +0200

    WIP access FTP server

--- appli.py
+++ appli.py
@@ -1,20 +1,38 @@
-from ftplib import FTP
+import ftplib
+import ssl
 
-ftp = FTP('192.168.1.14')
-ftp.login(user='lucas.chardin@hotmail.fr', passwd = 'LAROUSSE')
+class ImplicitFTP_TLS(ftplib.FTP_TLS):
+    """FTP_TLS subclass that automatically wraps sockets in SSL to support implicit FTPS."""
 
-ftp.cwd('/hvhgkcytkhdyrj/')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._sock = None
 
-print(ftp.retrlines('LIST')  )
+    @property
+    def sock(self):
+        """Return the socket."""
+        return self._sock
+
+    @sock.setter
+    def sock(self, value):
+        """When modifying the socket, ensure that it is ssl wrapped."""
+        if value is not None and not isinstance(value, ssl.SSLSocket):
+            value = self.context.wrap_socket(value)
+        self._sock = value
+
+ftp_client = ImplicitFTP_TLS()
+ftp_client.connect(host='89.156.226.85', port=2121, timeout=90)
+ftp_client.set_pasv(False)
+ftp_client.login(user='Lacanaux', passwd='Le92!jhGI')
+ftp_client.prot_p()
 
 def grabFile():
 
-    filename = 'TEST.txt'
+    filename = 'test.txt'
 
     localfile = open(filename, 'wb')
-    ftp.retrbinary('RETR ' + filename, localfile.write, 1024)
-
-    ftp.quit()
+    ftp_client.retrbinary('RETR ' + filename, localfile.write, 1024)
+    ftp_client.quit()
     localfile.close()
 
 
