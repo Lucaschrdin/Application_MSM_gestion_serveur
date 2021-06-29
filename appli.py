1705ce7a98c6648d3ecb8f63a984bac2bed1e5b8
Lucaschrdin <lucas.chardin@hotmail.fr>
2021-06-29 09:07:39 +0200

    connexion au serveur ftp faite

--- appli.py
+++ appli.py
@@ -1,39 +1,12 @@
-import ftplib
-import ssl
+from ftplib import FTP_TLS
+ftps = FTP_TLS('89.156.226.85')
+ftps.login('Lacanaux', 'Le92!jhGI')           # login anonymously before securing control channel
+ftps.prot_p()          # switch to secure data connection.. IMPORTANT! Otherwise, only the user and password is encrypted and not all the file data.
+ftps.retrlines('LIST')
 
-class ImplicitFTP_TLS(ftplib.FTP_TLS):
-    """FTP_TLS subclass that automatically wraps sockets in SSL to support implicit FTPS."""
+filename = 'test.txt'
+myfile = open(filename, 'wb')
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._sock = None
+ftps.retrbinary('RETR %s' % filename, myfile.write)
 
-    @property
-    def sock(self):
-        """Return the socket."""
-        return self._sock
-
-    @sock.setter
-    def sock(self, value):
-        """When modifying the socket, ensure that it is ssl wrapped."""
-        if value is not None and not isinstance(value, ssl.SSLSocket):
-            value = self.context.wrap_socket(value)
-        self._sock = value
-
-ftp_client = ImplicitFTP_TLS()
-ftp_client.connect(host='89.156.226.85', port=2121, timeout=90)
-ftp_client.set_pasv(False)
-ftp_client.login(user='Lacanaux', passwd='Le92!jhGI')
-ftp_client.prot_p()
-
-def grabFile():
-
-    filename = 'test.txt'
-
-    localfile = open(filename, 'wb')
-    ftp_client.retrbinary('RETR ' + filename, localfile.write, 1024)
-    ftp_client.quit()
-    localfile.close()
-
-
-grabFile()
+ftps.close()
