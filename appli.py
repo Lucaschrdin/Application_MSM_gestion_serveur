64641e3bbac626008f3fb1614ac1886b81c6023d
Lucaschrdin <lucas.chardin@hotmail.fr>
2021-06-29 09:14:11 +0200

    connexion au serveur ftp faite

--- appli.py
+++ appli.py
@@ -1,7 +1,7 @@
 from ftplib import FTP_TLS
 ftps = FTP_TLS('89.156.226.85')
-ftps.login('Lacanaux', 'Le92!jhGI')           # login anonymously before securing control channel
-ftps.prot_p()          # switch to secure data connection.. IMPORTANT! Otherwise, only the user and password is encrypted and not all the file data.
+ftps.login('Lacanaux', 'Le92!jhGI')
+ftps.prot_p() 
 ftps.retrlines('LIST')
 
 filename = 'test.txt'
